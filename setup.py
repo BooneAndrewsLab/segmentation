1dce3cd496b3c8a41b7b4fc0bef2bcd6ee9f999e
usajusaj <usajusaj@usajusaj.org>
2019-08-08 11:20:43 -0400

    Conda compat and some cleanup

--- setup.py
+++ setup.py
@@ -5,19 +5,12 @@ from Cython.Distutils import build_ext
 from setuptools import setup, Extension
 
 our_flags = ["-march=native", "-std=c++11", "-DNDEBUG", "-Ofast", "-Wcpp"]
-extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
-
-for f in extra_compile_args[:]:
-    if f.startswith('-march=') or f.startswith('-std=') or f.startswith('-O'):
-        extra_compile_args.remove(f)
-
-extra_compile_args += our_flags
 
 ext_modules = [
     Extension("segmentation.segmentation", ["segmentation/segmentation.pyx", ],
               language="c++",
-              include_dirs=[np.get_include()],
-              extra_compile_args=extra_compile_args,
+              include_dirs=[np.get_include(), sysconfig.get_config_var('INCLUDEDIR')],
+              extra_compile_args=our_flags,
               extra_link_args=["-std=c++11"],
               ),
 ]

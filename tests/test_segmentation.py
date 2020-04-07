c2f7e4ae98fda5666df14e11f73667b169ed0da9
usajusaj <usajusaj@usajusaj.org>
2020-04-07 18:18:25 -0400

    Fixing tests

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -4,6 +4,6 @@ import segmentation
 
 
 class SegmentationTestCase(unittest.TestCase):
-    def test_no_bam(self):
+    def test_imported(self):
         _ = segmentation.mixture_model  # just faking it for now
         self.assertTrue(True)
